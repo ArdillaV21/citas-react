1fd76509b2b494b9911a660c3bee81480f38e57c
ArdillaV21 <fedevalle21@gmail.com>
2024-01-24 19:39:58 -0300

    main

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+
+    return(
+        <header className="">
+            <h1>Desde Header</h1>
+            <p>Hola Mundo</p>
+        </header>    
+    )
+}
+
+export default Header
